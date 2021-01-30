cae804d40411908d927a551510d918c45cff56ac
Leonam Teixeira de Vasconcelos <leonan.aiirrc02t@gmail.com>
2021-01-30 16:06:59 -0300

    Adicionando tema no localStorage

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let wrapper = document.getElementById('wrapper')
+
 for ( let elem of wrapper.children ) 
     elem.addEventListener('click', (event) => { window.open ( elem.id + '/index.html', "_blank" ) })
 
@@ -7,15 +8,27 @@ const light_button = document.getElementById('light')
 
 const body = document.body
 
-dark_button.onclick = () => {
+const set_dark =  () => {
+    localStorage.setItem('theme', 'dark')
     body.classList.remove('light')
     body.classList.add('dark')
 }
 
-light_button.onclick = () => {
+const set_light = () => {
+    localStorage.setItem('theme', 'light')
     body.classList.remove('dark')
     body.classList.add('light')
 }
+
+let theme = localStorage.getItem('theme')
+
+if ( theme ) {
+    if ( theme == 'dark' ) set_dark()
+    else if ( theme == 'light' ) set_light()
+}
+
+dark_button.onclick = set_dark
+light_button.onclick = set_light
 
 const logo = document.getElementById('logo')
 logo.addEventListener('click', (event) => { window.open ( 'https://leonamtv.github.io/leonamtv/', '_blank' ) });
